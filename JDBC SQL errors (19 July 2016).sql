3b2d0e12f4b63bb44b4a721c743f1280c6e70814
Dmitry Dreyzin <dmitry.dreyzin@gmail.com>
2016-07-19 11:56:51 -0400

    SQL errors

---
+++ JDBC SQL errors (19 July 2016).sql
@@ -0,0 +1,40 @@
+--SQL Issue, 19 July 2016
+
+SELECT
+   S1."USERNAME" AS "USERNAME",
+   S1."COMMENT_BODY" AS "COMMENTBODY"
+FROM
+   "DATABASE"."USER_COMMENTS" S1;
+
+-- The above works.
+
+INSERT INTO "DATABASE"."USER_COMMENTS"
+   (USERNAME,
+   COMMENT_BODY)
+VALUES
+   ('Test.User.Me', 
+   'Test Comment');
+SELECT
+   S1."USERNAME" AS "USERNAME",
+   S1."COMMENT_BODY" AS "COMMENTBODY"
+FROM
+   "DATABASE"."USER_COMMENTS" S1;
+
+-- The above throws an error:
+-- [Oracle JDBC Driver][Oracle]ORA-00911: invalid character
+
+INSERT INTO "DATABASE"."USER_COMMENTS"
+   ([USERNAME],
+   [COMMENT_BODY])
+VALUES
+   ('Test.User.Me', 
+   'Test Comment');
+SELECT
+   S1."USERNAME" AS "USERNAME",
+   S1."COMMENT_BODY" AS "COMMENTBODY"
+FROM
+   "DATABASE"."USER_COMMENTS" S1;
+
+-- The above throws an error:
+-- [Oracle JDBC Driver][Oracle]ORA-00928: missing SELECT keyword
+ 
