41d698a9ad252b1f6d903f5ef0e5c34ca357715a
HalcyonicDays <white.zayetz@gmail.com>
2016-07-19 12:06:16 -0400

    Update JDBC SQL errors (19 July 2016).sql

--- JDBC SQL errors (19 July 2016).sql
+++ JDBC SQL errors (19 July 2016).sql
@@ -38,3 +38,17 @@ FROM
 -- The above throws an error:
 -- [Oracle JDBC Driver][Oracle]ORA-00928: missing SELECT keyword
  
+INSERT INTO "DATABASE"."USER_COMMENTS"
+ ("USERNAME" AS "USERNAME",
+  "COMMENT_BODY" AS "COMMENTBODY")
+VALUES
+ (%CURRENT_USER%,
+  ?Comment);
+SELECT
+   S1."USERNAME" AS "USERNAME",
+   S1."COMMENT_BODY" AS "COMMENTBODY"
+FROM
+   "DATABASE"."USER_COMMENTS" S1;
+
+-- The above throws an error:
+-- [Oracle JDBC Driver][Oracle]ORA-00917: missing comma
